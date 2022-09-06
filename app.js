b888bfebf3a950f413818a3ae0120432013de917
gary909 <garywhite61@hotmail.com>
2022-09-06 13:10:56 +0200

    code

---
+++ app.js
@@ -0,0 +1,6 @@
+function typeValidation(variable, type) {
+    // Your code should be here ;) 
+}
+
+console.log(typeValidation(42, "number")); // true
+console.log(typeValidation("42", "number")); // false
